61876b112af14e28117642b4905f53bb99c8110a
chris <christianpimpo304@hotmail.com>
2025-03-24 17:02:53 -0300

    post created

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
+using api.Mappers;
 using api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,20 +22,32 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Stock>>> GetAll()
+        public async Task<ActionResult<List<Stock>>> GetAllStocks()
         {
             var stocks = await _context.Stocks.ToListAsync();
+            var stocksDtos = stocks.Select(stock => stock.ToStockDto());
 
-            return Ok(stocks);
+            return Ok(stocksDtos);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Stock>> GetById([FromRoute] int id)
+        public async Task<ActionResult<Stock>> GetStockById([FromRoute] int id)
         {
             var stock = await _context.Stocks.FirstOrDefaultAsync() as Stock;
 
             if (stock == null) return NotFound("Invalid Id");
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Stock>> CreateStockRequest([FromBody] CreateStockDto stockModel)
+        {
+            var stock = stockModel.StockFromCreateStockDto();
+
+            await _context.Stocks.AddAsync(stock);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetStockById), new { id = stock.Id }, stock.ToStockDto());
         }
     }
 }
--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c1ae5750715df2ef278fd364cf4ebf13a9fa059")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
